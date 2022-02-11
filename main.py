c0eca5600502b0378dad67c2c7e4d85dfc43dd11
Darnokiv <85449868+Darnokiv@users.noreply.github.com>
2022-02-11 14:06:06 +0100

    Design Improvements

--- main.py
+++ main.py
@@ -23,46 +23,53 @@ class RootWindow:
         self.root_window.title(f"GIS Downloader")
         self.root_window.geometry("900x600+40+40")
         self.root_window.resizable(False, False)
+        self.root_window.iconphoto(True, tk.PhotoImage(file="srk_logo.png"))
         self.frm_main = tk.Frame()
         self.root_gui()
         self.root_window.mainloop()
 
     def root_gui(self):
-
+        self.root_window.configure(bg="#E6E6E6")
         self.frm_main = tk.Frame(
-            master=self.root_window
+            master=self.root_window,
+            bg="#E6E6E6"
         )
         lbl_welcome = tk.Label(
             master=self.frm_main,
-            font="Helvetica 30 bold",
+            bg="#E6E6E6",
+            font="Bahnschrift 30",
             text="GIS Downloader"
         )
         btn_variante1 = tk.Button(
             master=self.frm_main,
-            width=45,
-            height=9,
-            font=20,
+            bg="#F2F2F2",
+            width=37,
+            height=8,
+            font="Bahnschrift 15",
             text="Variante 1"
         )
         btn_variante2 = tk.Button(
             master=self.frm_main,
-            width=45,
-            height=9,
-            font=20,
+            bg="#F2F2F2",
+            width=37,
+            height=8,
+            font="Bahnschrift 15",
             text="Variante 2"
         )
         btn_variante3 = tk.Button(
             master=self.frm_main,
-            width=45,
-            height=9,
-            font=20,
+            bg="#F2F2F2",
+            width=37,
+            height=8,
+            font="Bahnschrift 15",
             text="Variante 3"
         )
         btn_variante4 = tk.Button(
             master=self.frm_main,
-            width=45,
-            height=9,
-            font=20,
+            bg="#F2F2F2",
+            width=37,
+            height=8,
+            font="Bahnschrift 15",
             text="Variante 4"
         )
 
@@ -73,7 +80,7 @@ class RootWindow:
             column=2, row=0, columnspan=10, rowspan=2, padx=300, pady=40
         )
         btn_variante1.grid(
-            column=2, row=2, padx=20, pady=30
+            column=2, row=2, padx=20, pady=15
         )
         btn_variante2.grid(
             column=3, row=2
